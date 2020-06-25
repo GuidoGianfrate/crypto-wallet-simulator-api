fbe54e8760507b4c6602c7495cbee848021a255b
Guido Gianfrate <guido.gianfrate@softvision.com>
2020-06-25 09:39:58 -0300

    transfer test in operation service

--- src/test/java/com/softvision/crypto/wallets/service/OperationCurrencyServiceTest.java
+++ src/test/java/com/softvision/crypto/wallets/service/OperationCurrencyServiceTest.java
@@ -1,6 +1,7 @@
 package com.softvision.crypto.wallets.service;
 
 import com.softvision.crypto.wallets.dto.TransferCurrencyDTO;
+import com.softvision.crypto.wallets.dto.TransferResumeDTO;
 import com.softvision.crypto.wallets.dto.WalletDTO;
 import com.softvision.crypto.wallets.exception.BadInputException;
 import com.softvision.crypto.wallets.exception.ElementNotFoundException;
@@ -118,6 +119,149 @@ class OperationCurrencyServiceTest {
                 });
     }
 
+    @Test
+    void buyWithNotExistentWallet(){
+
+        //the wallet not have EUR
+        when(operationCurrencyService.buyCurrency(anyString(),anyDouble(),anyString(),anyLong())).thenThrow(ElementNotFoundException.class);
+        assertThrows(ElementNotFoundException.class,
+                ()->{
+                    operationCurrencyService.buyCurrency("USD",123.0,"EUR",8L);
+                });
+    }
+
+    @Test
+    void transferMoneyBetweenWalletsWithSameMoney() {
+
+        //before transaction
+
+        HashMap coinPriceWallet1Before = new HashMap();
+        coinPriceWallet1Before.put("USD",100);
+
+        WalletDTO walletDTOInitialBefore = new WalletDTO();
+        walletDTOInitialBefore.setName("wallet one");
+        walletDTOInitialBefore.setId(1L);
+        walletDTOInitialBefore.setCoinPrice(coinPriceWallet1Before);
+
+        HashMap coinPriceWalletThatReceiveBefore = new HashMap();
+        coinPriceWalletThatReceiveBefore.put("USD",100);
+
+        WalletDTO walletDTOThatReceiveBefore = new WalletDTO();
+        walletDTOThatReceiveBefore.setName("wallet two");
+        walletDTOThatReceiveBefore.setId(2L);
+        walletDTOThatReceiveBefore.setCoinPrice(coinPriceWalletThatReceiveBefore);
+
+        //after transaction
+
+        HashMap coinPriceWallet1After = new HashMap();
+        coinPriceWallet1After.put("USD",50);
+
+        WalletDTO walletDTOInitialAfter = new WalletDTO();
+        walletDTOInitialAfter.setName("wallet one");
+        walletDTOInitialAfter.setId(1L);
+        walletDTOInitialAfter.setCoinPrice(coinPriceWallet1After);
+
+        HashMap coinPriceWalletThatReceiveAfter = new HashMap();
+        coinPriceWalletThatReceiveAfter.put("USD",150);
+
+        WalletDTO walletDTOThatReceiveAfter = new WalletDTO();
+        walletDTOThatReceiveAfter.setName("wallet two");
+        walletDTOThatReceiveAfter.setId(2L);
+        walletDTOThatReceiveAfter.setCoinPrice(coinPriceWalletThatReceiveAfter);
+
+        TransferResumeDTO transferResumeDTO = new TransferResumeDTO();
+        transferResumeDTO.setYourWallet(walletDTOInitialAfter);
+        transferResumeDTO.setReceiver(walletDTOThatReceiveAfter);
+
+
+        when(operationCurrencyService.transferMoneyBetweenWallets(anyLong(),anyString(),anyDouble(),
+                anyString(),anyLong())).thenReturn(transferResumeDTO);
+
+
+        TransferResumeDTO transferResumeDTOAfterTrasfer =   operationCurrencyService.transferMoneyBetweenWallets(walletDTOInitialBefore.getId(),
+                "USD",50.0,"USD",walletDTOThatReceiveBefore.getId());
+
+        WalletDTO walletDTOInitialFromService = transferResumeDTO.getYourWallet();
+        WalletDTO walletDTOReceiverFromService = transferResumeDTO.getReceiver();
+
+
+        Assert.assertTrue(walletDTOInitialFromService.getId()==walletDTOInitialBefore.getId());
+        Assert.assertTrue(walletDTOReceiverFromService.getId() == walletDTOThatReceiveBefore.getId());
+
+        Assert.assertTrue(walletDTOInitialFromService.getCoinPrice().get("USD") != walletDTOInitialBefore.getCoinPrice().get("USD"));
+        Assert.assertTrue(walletDTOReceiverFromService.getCoinPrice().get("USD") != walletDTOThatReceiveBefore.getCoinPrice().get("USD"));
+
+
+    }
+
+    @Test
+    void transferMoneyBetweenWalletsWithDifferent() {
+
+        //before transaction
+
+        HashMap coinPriceWallet1Before = new HashMap();
+        coinPriceWallet1Before.put("USD",100.0);
+
+        WalletDTO walletDTOInitialBefore = new WalletDTO();
+        walletDTOInitialBefore.setName("wallet one");
+        walletDTOInitialBefore.setId(1L);
+        walletDTOInitialBefore.setCoinPrice(coinPriceWallet1Before);
+
+        HashMap coinPriceWalletThatReceiveBefore = new HashMap();
+        coinPriceWalletThatReceiveBefore.put("USD",100.0);
+
+        WalletDTO walletDTOThatReceiveBefore = new WalletDTO();
+        walletDTOThatReceiveBefore.setName("wallet two");
+        walletDTOThatReceiveBefore.setId(2L);
+        walletDTOThatReceiveBefore.setCoinPrice(coinPriceWalletThatReceiveBefore);
+
+        //after transaction
+
+        HashMap coinPriceWallet1After = new HashMap();
+        coinPriceWallet1After.put("USD",50.0);
+
+        WalletDTO walletDTOInitialAfter = new WalletDTO();
+        walletDTOInitialAfter.setName("wallet one");
+        walletDTOInitialAfter.setId(1L);
+        walletDTOInitialAfter.setCoinPrice(coinPriceWallet1After);
+
+        HashMap coinPriceWalletThatReceiveAfter = new HashMap();
+        coinPriceWalletThatReceiveAfter.put("USD",100.0);
+        coinPriceWalletThatReceiveAfter.put("ARS",6800.0);
+
+        WalletDTO walletDTOThatReceiveAfter = new WalletDTO();
+        walletDTOThatReceiveAfter.setName("wallet two");
+        walletDTOThatReceiveAfter.setId(2L);
+        walletDTOThatReceiveAfter.setCoinPrice(coinPriceWalletThatReceiveAfter);
+
+        TransferResumeDTO transferResumeDTO = new TransferResumeDTO();
+        transferResumeDTO.setYourWallet(walletDTOInitialAfter);
+        transferResumeDTO.setReceiver(walletDTOThatReceiveAfter);
+
+
+        when(operationCurrencyService.transferMoneyBetweenWallets(anyLong(),anyString(),anyDouble(),
+                anyString(),anyLong())).thenReturn(transferResumeDTO);
+
+
+        TransferResumeDTO transferResumeDTOAfterTrasfer =   operationCurrencyService.transferMoneyBetweenWallets(walletDTOInitialBefore.getId(),
+                "USD",50.0,"ARS",walletDTOThatReceiveBefore.getId());
+
+        WalletDTO walletDTOInitialFromService = transferResumeDTO.getYourWallet();
+        WalletDTO walletDTOReceiverFromService = transferResumeDTO.getReceiver();
+
+
+        Assert.assertTrue(walletDTOInitialFromService.getId()==walletDTOInitialBefore.getId());
+        Assert.assertTrue(walletDTOReceiverFromService.getId() == walletDTOThatReceiveBefore.getId());
+
+        Assert.assertTrue(walletDTOInitialFromService.getCoinPrice().get("USD") != walletDTOInitialBefore.getCoinPrice().get("USD"));
+        System.out.println(walletDTOReceiverFromService.getCoinPrice().get("USD"));
+        System.out.println(walletDTOThatReceiveBefore.getCoinPrice().get("USD"));
+        Assert.assertTrue(walletDTOReceiverFromService.getCoinPrice().get("USD").equals(walletDTOThatReceiveBefore.getCoinPrice().get("USD")));
+        Assert.assertTrue(walletDTOReceiverFromService.getCoinPrice().get("ARS") != null);
+
+
+    }
+
 
 
 
@@ -136,7 +280,7 @@ class OperationCurrencyServiceTest {
         walletDTOInitial.setCoinPrice(coinPriceWallet1);
 
         HashMap coinPriceWalletThatReceive = new HashMap();
-        coinPriceWallet1.put("USD",100);
+        coinPriceWalletThatReceive.put("USD",100);
 
         WalletDTO walletDTOThatReceive = new WalletDTO();
         walletDTOThatReceive.setName("wallet two");
@@ -147,8 +291,107 @@ class OperationCurrencyServiceTest {
                                                                     anyString(),anyLong())).thenThrow(BadInputException.class);
         assertThrows(BadInputException.class,
                 ()->{
-                    operationCurrencyService.buyCurrency("ehfwif",123.0,"EUR",1L);
+                    operationCurrencyService.transferMoneyBetweenWallets(walletDTOInitial.getId(),
+                            "ARS",234234.0,"USD",walletDTOThatReceive.getId());
                 });
 
+    }
+
+    @Test
+    void transferNotExistentMoneyMoneyBetweenWallets() {
+
+        HashMap coinPriceWallet1 = new HashMap();
+        coinPriceWallet1.put("USD",100);
+
+        WalletDTO walletDTOInitial = new WalletDTO();
+        walletDTOInitial.setName("wallet one");
+        walletDTOInitial.setId(1L);
+        walletDTOInitial.setCoinPrice(coinPriceWallet1);
+
+        HashMap coinPriceWalletThatReceive = new HashMap();
+        coinPriceWalletThatReceive.put("USD",100);
+
+        WalletDTO walletDTOThatReceive = new WalletDTO();
+        walletDTOThatReceive.setName("wallet two");
+        walletDTOThatReceive.setId(2L);
+        walletDTOThatReceive.setCoinPrice(coinPriceWalletThatReceive);
+
+        when(operationCurrencyService.transferMoneyBetweenWallets(anyLong(),anyString(),anyDouble(),
+                anyString(),anyLong())).thenThrow(BadInputException.class);
+        assertThrows(BadInputException.class,
+                ()->{
+                    operationCurrencyService.transferMoneyBetweenWallets(walletDTOInitial.getId(),
+                            "fewfwef",234234.0,"USD",walletDTOThatReceive.getId());
+                });
+
+    }
+
+    @Test
+    void transferMoneyBetweenWalletsAndReceiveNotExistentMoney() {
+
+        HashMap coinPriceWallet1 = new HashMap();
+        coinPriceWallet1.put("USD",100);
+
+        WalletDTO walletDTOInitial = new WalletDTO();
+        walletDTOInitial.setName("wallet one");
+        walletDTOInitial.setId(1L);
+        walletDTOInitial.setCoinPrice(coinPriceWallet1);
+
+        HashMap coinPriceWalletThatReceive = new HashMap();
+        coinPriceWalletThatReceive.put("USD",100);
+
+        WalletDTO walletDTOThatReceive = new WalletDTO();
+        walletDTOThatReceive.setName("wallet two");
+        walletDTOThatReceive.setId(2L);
+        walletDTOThatReceive.setCoinPrice(coinPriceWalletThatReceive);
+
+        when(operationCurrencyService.transferMoneyBetweenWallets(anyLong(),anyString(),anyDouble(),
+                anyString(),anyLong())).thenThrow(BadInputException.class);
+        assertThrows(BadInputException.class,
+                ()->{
+                    operationCurrencyService.transferMoneyBetweenWallets(walletDTOInitial.getId(),
+                            "USD",234234.0,"fewfe",walletDTOThatReceive.getId());
+                });
+
+    }
+
+    @Test
+    void transferMoneyBetweenWalletsAndTheDepositorNotExist() {
+
+        HashMap coinPriceWalletThatReceive = new HashMap();
+        coinPriceWalletThatReceive.put("USD",100);
+
+        WalletDTO walletDTOThatReceive = new WalletDTO();
+        walletDTOThatReceive.setName("wallet two");
+        walletDTOThatReceive.setId(2L);
+        walletDTOThatReceive.setCoinPrice(coinPriceWalletThatReceive);
+
+        when(operationCurrencyService.transferMoneyBetweenWallets(anyLong(),anyString(),anyDouble(),
+                anyString(),anyLong())).thenThrow(ElementNotFoundException.class);
+        assertThrows(ElementNotFoundException.class,
+                ()->{
+                    operationCurrencyService.transferMoneyBetweenWallets((8L),
+                            "USD",234234.0,"fewfe",2L);
+                });
+    }
+
+    @Test
+    void transferMoneyBetweenWalletsAndTheReceiverNotExist() {
+
+        HashMap coinPriceWallet1 = new HashMap();
+        coinPriceWallet1.put("USD", 100);
+
+        WalletDTO walletDTOInitial = new WalletDTO();
+        walletDTOInitial.setName("wallet one");
+        walletDTOInitial.setId(1L);
+        walletDTOInitial.setCoinPrice(coinPriceWallet1);
+
+        when(operationCurrencyService.transferMoneyBetweenWallets(anyLong(), anyString(), anyDouble(),
+                anyString(), anyLong())).thenThrow(ElementNotFoundException.class);
+        assertThrows(ElementNotFoundException.class,
+                () -> {
+                    operationCurrencyService.transferMoneyBetweenWallets(1L,
+                            "USD", 234234.0, "fewfe", 9L);
+                });
     }
 }
